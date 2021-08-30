425564e35e0c99d8411ff71078f34a09253799a2
zeeshan13026 <shan.13026@gmail.com>
2021-08-31 03:09:14 +0530

    html report code commentted

--- Test/conftest.py
+++ Test/conftest.py
@@ -4,13 +4,13 @@ import datetime
 import pytest
 from selenium import webdriver
 
-def pytest_cmdline_preparse(config, args):
-    curr_time = datetime.datetime.now()
-    # 2020_10_09_23_48_57.3294873849
-    curr = str(curr_time).replace("-", "_").replace(" ", "_").replace(":", "_")
-    #html_file = os.getcwd() + "/../Reports/" +get_current_date_time()  + "ReportFile.html"
-    html_file = os.getcwd() + "/../Main/Reports/" + curr.split(".")[0] + "ReportFile.html"
-    args.extend(['--html', html_file, '--self-contained-html'])
+# def pytest_cmdline_preparse(config, args):
+#     curr_time = datetime.datetime.now()
+#     # 2020_10_09_23_48_57.3294873849
+#     curr = str(curr_time).replace("-", "_").replace(" ", "_").replace(":", "_")
+#     #html_file = os.getcwd() + "/../Reports/" +get_current_date_time()  + "ReportFile.html"
+#     html_file = os.getcwd() + "/../Main/Reports/" + curr.split(".")[0] + "ReportFile.html"
+#     args.extend(['--html', html_file, '--self-contained-html'])
     #py.test --html:html_file
 
 @pytest.fixture(autouse=True)
